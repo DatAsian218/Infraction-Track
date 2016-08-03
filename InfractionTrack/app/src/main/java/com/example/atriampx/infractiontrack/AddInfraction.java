423118f0e44689c209dcb5f39d6050fd5c7dc5dd
Triampol <alexandra.lesliex.triampol@intel.com>
2016-08-03 17:11:16 -0500

    Infraction Track app

---
+++ InfractionTrack/app/src/main/java/com/example/atriampx/infractiontrack/AddInfraction.java
@@ -0,0 +1,21 @@
+package com.example.atriampx.infractiontrack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class AddInfraction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_infraction);
+    }
+
+}
